f0ddb88e4bc9e6f884f0cdfaeeaeacde17ab2b53
Else32 <nick.tollner@icloud.com>
2022-05-23 23:42:26 +0930

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Nickt on 23/05/2022.
+//
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    /**
+     * Read in each line from file
+     *
+     * Split around comma delimiter
+     *
+     * Split boards into array of arrays?
+     *
+     */
+
+    std::ifstream inputFile;
+
+
+
+
+}
